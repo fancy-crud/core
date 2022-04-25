6bcf2e1fbbc67f080ef7f05c2255ea6b847855db
Christopher A. Flores <kershingf@gmail.com>
2022-04-24 18:26:34 -0600

    feat: add support to tooltip message

--- src/components/utils/FButtonIcon.vue
+++ src/components/utils/FButtonIcon.vue
@@ -1,5 +1,6 @@
 <template>
   <button
+    ref="buttonRef"
     type="button"
     class="text-primary-700 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-primary-500 dark:text-primary-500 dark:hover:text-white dark:focus:ring-primary-800"
     v-bind="$attrs"
@@ -9,15 +10,44 @@
       :class="[icon, iconSize]"
     />
   </button>
+
+  <teleport to="body">
+    <div
+      ref="tooltipRef"
+      role="tooltip"
+      class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700"
+    >
+      {{ tooltip }}
+      <div
+        class="tooltip-arrow"
+        data-popper-arrow
+      />
+    </div>
+  </teleport>
 </template>
 
 <script lang="ts" setup>
 const props = defineProps<{
   icon: string
   size?: string
+  tooltip?: string
+  tooltipPlacement?: string
 }>()
+
+const buttonRef = ref()
+const tooltipRef = ref()
+const tooltipPopper = ref()
 
 const iconSize = computed(() => {
   return props.size ?? 'text-2xl'
+})
+
+onMounted(() => {
+  if (props.tooltip) {
+    tooltipPopper.value = new Tooltip(tooltipRef.value, buttonRef.value, {
+      triggerType: 'hover',
+      placement: props.tooltipPlacement ?? 'bottom',
+    })
+  }
 })
 </script>
