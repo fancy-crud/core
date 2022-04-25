88508733425906d1232809812d36f6fdb3b2b1d7
Christopher A. Flores <kershingf@gmail.com>
2022-04-24 19:14:01 -0600

    style: remove border top to prevent doble divider in table

--- src/components/table/FTableFooter.vue
+++ src/components/table/FTableFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card-actions w-full flex flex-nowrap justify-between items-center p-4 border-t-[1px] border-slate-100">
+  <div class="card-actions w-full flex flex-nowrap justify-between items-center p-4">
     <slot />
   </div>
 </template>
