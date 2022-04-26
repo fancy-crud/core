79937cc175ccf50dd7173d2261e6ea0be725e8d5
Christopher A. Flores <kershingf@gmail.com>
2022-04-25 19:54:44 -0600

    feat: add styles to current selected page

--- src/components/table/FPagination.vue
+++ src/components/table/FPagination.vue
@@ -40,7 +40,8 @@
         @click="onSelectPage(p)"
       >
         <button
-          class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          class="py-2 px-3 leading-tight border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          :class="currentPageClass(p, currentPage)"
         >
           {{ p }}
         </button>
@@ -114,6 +115,9 @@ const onSelectPage = (page: number) => {
 
 const includeFirstPage = computed(() => !pager.value.pages.includes(1))
 const includeLastPage = computed(() => !pager.value.pages.includes(pager.value.totalPages))
+const currentPageClass = computed(() => (page: number, currentPage: number) => {
+  return page === currentPage ? 'text-blue-600 bg-blue-50' : 'text-gray-500 bg-white'
+})
 
 watch(() => props.modelValue, () => currentPage.value = props.modelValue)
 
