7dfbe9fe5ddebb69526e97a994f8c89c6827270f
Christopher A. Flores <kershingf@gmail.com>
2022-04-25 21:53:23 -0600

    feat: allow table checkbox to get record value

--- src/components/table/FTableRow.vue
+++ src/components/table/FTableRow.vue
@@ -14,6 +14,7 @@
           <input
             type="checkbox"
             class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            :checked="row[header.value]"
           >
         </div>
       </template>
