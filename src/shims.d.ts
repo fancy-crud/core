5db845bb0a8e3c8082a0f169b2398c6744c47297
Christopher A. Flores <kershingf@gmail.com>
2022-04-30 17:12:51 -0600

    style: add typescript support to used flowbite classes

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,8 +1,26 @@
+// export default Dismiss
+
 declare interface Window {
   // extend the window
 }
-
 declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
+}
+
+
+declare class Dismiss {
+  constructor(targetEl: any, options?: any): void
+  hide(): void
+}
+
+declare class Tooltip {
+  constructor(targetEl: any, triggerEl: any, options?: any): void
+  hide(): void
+}
+
+declare class Modal {
+  constructor(targetEl: any, options?: any): void
+  show(): void
+  hide(): void
 }
