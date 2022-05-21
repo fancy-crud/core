7ce745b48245a7f70c68c571c487368ce75d7759
Christopher A. Flores <kershingf@gmail.com>
2022-05-21 09:04:08 -0600

    fix: display popup modal on FInputDate and add action buttons
    #2xpfw3d

--- src/components/inputs/FInputDate/FInputDate.vue
+++ src/components/inputs/FInputDate/FInputDate.vue
@@ -9,19 +9,21 @@
           fill="currentColor"
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
-        ><path
-          fill-rule="evenodd"
-          d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-          clip-rule="evenodd"
-        /></svg>
+        >
+          <path
+            fill-rule="evenodd"
+            d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+            clip-rule="evenodd"
+          />
+        </svg>
       </div>
       <input
-        v-model="modelValue"
-        v-bind="field"
+        @blur="setModel"
+        ref="inputRef"
         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        datepicker-format="dd/mm/yyyy"
+        placeholder="Select datetime"
         type="text"
-        placeholder="Select date"
-        datepicker
       >
     </div>
 
@@ -30,9 +32,9 @@
 </template>
 
 <script lang="ts" setup>
+import Datepicker from 'flowbite-datepicker/Datepicker';
 import type { NormalizedFieldStructure } from '@/types'
 import { setInputTextModelValue, useSetModelValue } from '@/composables'
-// import 'flowbite/dist/datepicker.js'
 
 const props = defineProps<{
   field: NormalizedFieldStructure
@@ -44,9 +46,27 @@ const emit = defineEmits<{
 
 provide('field', props.field)
 
+const inputRef = ref<HTMLInputElement>()
+const dateInput = ref<any>()
 const modelValue = useSetModelValue(props.field, () => {
   setInputTextModelValue(props.field, modelValue.value)
   emit('update:modelValue', modelValue.value)
 })
 
+const setModel = (e: Event) => {
+  const value = (e.target as HTMLInputElement).value
+  modelValue.value = value
+}
+
+onMounted(() => {
+ if (inputRef.value) {
+    dateInput.value = new Datepicker(inputRef.value, {
+      todayHighlight: true,
+      todayBtn: true,
+      clearBtn: true,
+    });
+  }
+})
+
+onUnmounted(() => dateInput.value.hide())
 </script>
