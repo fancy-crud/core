1789bed9c6c865666a653cf3da4f5b8783ef1e83
Christopher A. Flores <christopher.flores@codexitos.com>
2023-08-22 09:36:28 -0600

    ci: update branches

--- .releaserc.js
+++ .releaserc.js
@@ -1,3 +1,10 @@
 {
-  branches: ['major']
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true }
+  ]
 }
