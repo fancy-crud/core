eab3a1f8a59253d33a147b83d1aef74711149dfd
Christopher A. Flores <christopher.flores@codexitos.com>
2023-11-19 08:45:46 -0600

    fix: auto add lookup field in table settings

--- packages/core/src/tables/capabilities/normalize-table-settings.ts
+++ packages/core/src/tables/capabilities/normalize-table-settings.ts
@@ -2,6 +2,10 @@ import type { INormalizeTableSettingsHandler, NormalizeTableSettingsCommand, Nor
 
 export class NormalizeTableSettingsHandler implements INormalizeTableSettingsHandler {
   execute<T extends RawTableSettings>({ rawSettings }: NormalizeTableSettingsCommand<T>): T & NormalizedTableSettings {
-    return rawSettings as T & NormalizedTableSettings
+    const _settings = {
+      lookupField: rawSettings?.lookupField || 'id',
+    } as T & NormalizedTableSettings
+
+    return _settings
   }
 }
