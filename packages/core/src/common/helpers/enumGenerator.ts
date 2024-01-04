0648b15bbc06c3072a79faef0f777421e99c6bde
Christopher A. Flores <christopher.flores@codexitos.com>
2024-01-03 20:38:02 -0600

    feat: add enumGenerator key value

--- packages/core/src/common/helpers/enumGenerator.ts
+++ packages/core/src/common/helpers/enumGenerator.ts
@@ -6,3 +6,12 @@ export function enumGenerator<T extends string>(arr: T[]): { [k in T]: k } {
 
   return Object.freeze(result)
 }
+
+export function enumGeneratorKeyValue<T extends string, U = unknown>(arr: [T, U][]): Record<T, U> {
+  const result = {} as Record<T, U>
+
+  for (const [key, value] of arr)
+    result[key] = value
+
+  return result
+}
