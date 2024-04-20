95b6834ba200b817eb65d3479ad290addbb66f74
Christopher A. Flores <christopher.flores@codexitos.com>
2024-04-20 14:13:30 -0600

    feat: implement reactivity proxies on form

--- demo-element-plus/src/viewer/FInputViewer.vue
+++ demo-element-plus/src/viewer/FInputViewer.vue
@@ -2,31 +2,26 @@
   <div class="bg-white rounded-xl p-4">
     <f-form @success="printing" @error="printing" v-bind="form" class="el-form--label-top" />
   </div>
-
-  <button @click="loadData">
-    Load data
-  </button>
+  <input v-model="title">
 </template>
 
 <script lang='ts' setup>
 // import { email, string } from 'valibot'
 import { FieldType, useForm } from '@fancy-crud/vue'
-import { Bus, LoadRemoteRecordCommand, ResetFieldsByFormIdCommand } from '@fancy-crud/core'
+import { Bus, ResetFieldsByFormIdCommand } from '@fancy-crud/core'
 
 const bus = new Bus()
 
-const settings = {
-  url: 'artists/',
-  title: 'Modo root',
-}
+const title = ref('Modo root')
 
 const form = useForm({
-  fields: {
+  fields: () => ({
     name: {
       type: FieldType.text,
       label: 'First name',
       placeholder: 'Como asi pues?',
       hintText: 'Display some message',
+      modelValue: title.value,
       // rules: value => ({ value, rule: string([email()]) }),
       wrapper: {
         class: 'col-span-6',
@@ -36,11 +31,14 @@ const form = useForm({
       type: '',
       modelValue: 'm',
     },
-  },
-  settings,
-  buttons: {
+  }),
+  settings: () => ({
+    url: 'artists/',
+    title: title.value,
+  }),
+  buttons: () => ({
     main: {
-      label: '{{ Lanzar | Modificar }}',
+      label: title.value,
       loading: false,
     },
     aux: {
@@ -50,21 +48,21 @@ const form = useForm({
         )
       },
     },
-  },
+  }),
 })
 
-function loadData(e: any) {
-  bus.execute(
-    new LoadRemoteRecordCommand(form.id, '9', {
-      onInit() {
-        form.buttons.main.loading = true
-      },
-      onFinally() {
-        form.buttons.main.loading = false
-      },
-    }),
-  )
-}
+// function loadData(e: any) {
+//   bus.execute(
+//     new LoadRemoteRecordCommand(form.id, '9', {
+//       onInit() {
+//         form.buttons.main.loading = true
+//       },
+//       onFinally() {
+//         form.buttons.main.loading = false
+//       },
+//     }),
+//   )
+// }
 
 function printing(response: any) { console.log(response) }
 </script>
