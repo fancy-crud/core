fab8316057bbe25268fdc0151b71eae3943710e2
Christopher A. Flores <christopher.flores@codexitos.com>
2025-10-10 22:40:24 -0600

    chore: update dependencies and add new demo applications
    - Upgraded package versions including pnpm, Tailwind CSS, and various Vue-related packages.
    - Removed deprecated PostCSS and Tailwind configuration files in favor of updated styles.
    - Introduced new demo applications for PrimeVue, Quasar, and Vuetify, each with their own configurations.
    - Updated existing demo applications to align with the latest package standards and practices.
    - Enhanced auto-imports and TypeScript configurations across multiple packages.

---
+++ demo-primevuex/src/viewer/FCheckboxViewer.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <f-form v-bind="form" />
+  </div>
+
+  <div class="p-10 mt-10 border">
+    {{ form.fields.terms.modelValue }}
+  </div>
+
+  <button @click="resetFields" class="px-8 py-4 bg-primary-500 text-white font-bold">
+    Reset
+  </button>
+
+  <div class="pt-10">
+    <f-checkbox :form-id="form.id" :field="form.fields.checkbox" />
+  </div>
+</template>
+
+<script lang='ts' setup>
+import { FCheckbox, useForm } from '@fancy-crud/vue'
+import { Bus, ResetFieldsByFormIdCommand } from '@fancy-crud/core'
+
+const options = [
+  { label: 'Option 1', value: 'option-1' },
+  { label: 'Option 2', value: 'option-2' },
+  { label: 'Option 3', value: 'option-3' },
+]
+
+const bus = new Bus()
+
+const form = useForm({
+  id: 'checkbox-field',
+  fields: {
+    terms: {
+      label: 'By clicking this checkbox, you agree to our terms and conditions',
+      type: 'checkbox',
+      inRow: true,
+      // modelValue: false,
+    },
+    checkbox: {
+      label: 'Checkbox',
+      type: 'checkbox',
+      inRow: true,
+      options: [true],
+    },
+    artists: {
+      label: 'Artists',
+      type: 'checkbox',
+      multiple: true,
+      optionLabel: 'label',
+      optionValue: 'value',
+      options,
+      modelValue: ['option-1'],
+    },
+    artists2: {
+      label: 'Artists 2',
+      type: 'checkbox',
+      multiple: true,
+      optionLabel: 'name',
+      optionValue: 'id',
+      url: 'artists/',
+      modelValue: ['option-1'],
+    },
+  },
+  settings: {
+    url: 'artists/',
+  },
+})
+
+function resetFields() {
+  bus.execute(
+    new ResetFieldsByFormIdCommand(form.id),
+  )
+}
+</script>
---
+++ demo-primevuex/src/viewer/FColorViewer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <f-form v-bind="form" />
+  </div>
+
+  <div class="p-10 mt-10 border">
+    {{ form.fields.color.modelValue }}
+  </div>
+</template>
+
+<script lang='ts' setup>
+import { useForm } from '@fancy-crud/vue'
+
+const form = useForm({
+  id: 'color-field',
+  fields: {
+    color: {
+      label: 'Color',
+      type: 'color',
+      hideCanvas: true,
+      hideSliders: true,
+      flat: true,
+      elevation: 0,
+      wrapper: {
+        class: 'col-span-12 full-width',
+      },
+    },
+  },
+  settings: {
+    url: 'artists/',
+  },
+})
+</script>
